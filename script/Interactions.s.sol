3f39047fe8bef941ef176d192068789925478d2f
smahdi-dev <seyedmahdi.hosseini24@gmail.com>
2024-04-22 16:55:26 +0330

    MintMoodNft and FlipMoodNft added to Interaction.s.sol

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "../src/BasicNft.sol";
+import {MoodNft} from "../src/MoodNft.sol";
 
 contract MintBasicNft is Script {
     string public constant PUG =
@@ -21,6 +22,40 @@ contract MintBasicNft is Script {
     function mintNftOnContract(address contractAddress) public {
         vm.startBroadcast();
         BasicNft(contractAddress).mintNft(PUG);
+        vm.stopBroadcast();
+    }
+}
+
+contract MintMoodNft is Script {
+    function run() external {
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment(
+            "MoodNft",
+            block.chainid
+        );
+
+        mintNftOnContract(mostRecentDeployed);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        MoodNft(contractAddress).mintNft();
+        vm.stopBroadcast();
+    }
+}
+
+contract FlipMoodNft is Script {
+    function run() external {
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment(
+            "MoodNft",
+            block.chainid
+        );
+
+        flipMoodNftOnContract(mostRecentDeployed);
+    }
+
+    function flipMoodNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        MoodNft(contractAddress).flipMood(0);
         vm.stopBroadcast();
     }
 }
